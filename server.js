c778af862761cefe80eff8fac8ed8fc9208994c7
fharaujo <faraujodeveloper@hotmail.com>
2021-09-09 12:48:57 -0300

    install dotenv and create server

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
+// importação das dependencias
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+// criando o app
+const app = express();
+const port = 3000;
+
+// utilizando o json
+app.use(express.json())
+
+// Servidor
+app.listen(port, () => {
+  try {
+    console.info(`Servidor rodando em: http://localhost:${port}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
