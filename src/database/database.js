f80839f55bb74dab90b6848f0d279e25b59ee28c
fharaujo <faraujodeveloper@hotmail.com>
2021-09-09 19:20:18 -0300

    database connect refeactory e and Schema disc create

--- src/database/database.js
+++ src/database/database.js
@@ -1,22 +1,34 @@
 // importação do mongoose
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 // função para conexão com o banco de dados
 const connectDB = () => {
-  mongoose.connect(
-    "string de conexão",
-    {
-      useNewUrlParse: true,
-      useUnifiedTopology: true,
-    },
-    (error) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("... MongoDB conectado com sucesso.");
+
+   
+    const connectToDb = () => {
+      const dbUser = process.env.DB_USER;
+      const dbPassword = process.env.DB_PASSWORD;
+      const dbName = process.env.DB_NAME;
+      const dbChar = process.env.DB_CHAR;
+      const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+      const options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      };
+      const client = new mongodb.MongoClient(connectionString, options);
+    //   const db = client.db("db_disc");
+    //   const personagens = db.collection("disc");
+    
+      async function conexao() {
+        try {
+          await client.connect();
+          console.log("Connected to MongoDB");
+        } catch (err) {
+          console.log(err);
+        }
       }
-    }
-  );
+    };
 };
 
 module.exports = connectDB;
