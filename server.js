a8402e97f6be53bbe0e5bd0027d9b1960907f650
fharaujo <faraujodeveloper@hotmail.com>
2021-09-10 17:25:16 -0300

    url server swagger

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://api-rapnacional.herokuapp.com/",
       },
     ],
     basePath: "/",
