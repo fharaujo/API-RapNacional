9b3a564dfb63218d654a75c29f2ba64680ade49e
fharaujo <faraujodeveloper@hotmail.com>
2021-09-13 18:20:19 -0300

    refactoy filter group swagger

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -175,7 +175,7 @@ routes.delete("/discs/:id", DiscMiddleware.isValidId, DiscController.remove);
  * paths:
  *    /filterByNameDisc:
  *        get:
- *          tags: Query Filters
+ *          tags: [Query Filters]
  *          summary: Get filter By name
  *          parameters:
  *            - in: query
@@ -197,7 +197,7 @@ routes.delete("/discs/:id", DiscMiddleware.isValidId, DiscController.remove);
  * 
  * /filterByArtistName:
  *        get:
- *          tags: Query Filters
+ *          tags: [Query Filters]
  *          summary: Get filter By Artist name
  *          parameters:
  *            - in: query
